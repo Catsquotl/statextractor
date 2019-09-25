99ea03fcb533a1a13b478b80e20a5a2845199f09
Catsquotl <catsquotl@gmail.com>
2019-09-25 06:48:09 +0200

    index work

--- statextractor.py
+++ statextractor.py
@@ -3,16 +3,21 @@ import re
 
 pdf = open("/home/catsquotl/Desktop/dnd5/Monster-Manual.pdf", 'rb')
 read_pdf = PyPDF4.PdfFileReader(pdf)
+index = (351,352)
+def get_index(pagenum):
+    page = read_pdf.getPage(pagenum)
+    rtxt = page.extractText()
+    santxt = rtxt.replace('\n', '')
+    ftxt = santxt.replace(',',':')
+    tmp_lis = re.findall(r'[\sA-z]* \w+: \d+', ftxt)
+    lis = []
+    for l in tmp_lis:
+        l = re.sub(r'[A-Z] ',' ',l)
+        l = l.lstrip()
+        lis.append(l)
+    return lis
 
-info = read_pdf.getNumPages()
-page = read_pdf.getPage(351)
+for n in index:
+    print(get_index(n))
 
-rtxt = page.extractText()
-santxt = rtxt.replace('\n', '')
-ftxt = santxt.replace(',',':')
-
-lis = re.findall(r'[\sA-z]* \w+: \d+', ftxt)
-
-for l in lis:
-    l = re.sub(r'[A-Z] ',' ',l)
-    print(l)
+#if __name__ == '__main__':
